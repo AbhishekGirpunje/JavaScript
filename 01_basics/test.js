348bafa69a05ecbe98b49ec1a2a45cd3144ad1a8
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-12 02:54:03 +0000

    var, let and const

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Abhishek")
+const EmailID= "abhi@gmail.com"
+let Name = "Abhishek"
+var RandomNumbers = 1234
+MobileNO = 981729
+
+let city;
+
+
+
+RandomNumbers = 2314
+Name = "sahil"
+MobileNO = 9876543
+city = "Amravati"
+
+
+console.table([EmailID, Name, RandomNumbers, MobileNO, city])
+
