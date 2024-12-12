adc0078628d9f6f901982d26a35cc7f5686909b6
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-12 04:20:11 +0000

    Dataypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // strict mode on
+
+// alert(1+1) We are using node.js, not browser
+
+// tc39 and mdn (For more information)
+
+// code readability should be high 
+
+let name = "Abhishek"
+let age = 20
+let isLoggedIn = false
+
+/*
+number - 2 to power 53
+bigint - for big website, applications like reddit, discord, trading.
+string - "  "
+boolean - true/false
+null - standalone value(type) empty value.
+undefined - not assign value
+symbol - unique
+*/
+
+
+// object
+
+console.log(typeof "Abhishek");// string type
+
+console.log(typeof null); // Its a object
+
+console.log(typeof undefined);// Its a type
+
+
