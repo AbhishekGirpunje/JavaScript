098c04f78bc26604cffeb17e9d35203d618e2585
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-13 01:07:10 +0000

    Operators

---
+++ 01_basics/Operators.js
@@ -0,0 +1,32 @@
+/*
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3%3); 
+console.log(3**3); //power
+console.log(3/2);
+*/
+
+/*
+let str1 = "Abhishek "
+let str2 = "Hitesh"
+
+console.log(str1 + str2); // we can't substract two strings
+*/
+
+
+// console.log(1 + true); //o/p- 2
+// console.log(1 + false); //o/p- 1
+
+// console.log( 1 + "2");
+// console.log( "1" + "2");
+// console.log( "1" + 2);
+// console.log( 1 + 2 + "3");
+
+// console.log(3 + 1 -3 * 9 % 2);// not good code use parentheses
+
+let gameCounter = 1
+++gameCounter
+console.log(gameCounter);
+
+
