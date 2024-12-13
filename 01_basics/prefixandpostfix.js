f57c03833122a9c6279766d7eb188c71482c0d49
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-13 19:23:01 +0000

    Prefix and Postfix

---
+++ 01_basics/prefixandpostfix.js
@@ -0,0 +1,23 @@
+let myNumber = 1
+
+let a = myNumber++  //use than change(postfix)
+
+console.log(a)
+
+let nextNumber = 1
+
+let b =  ++nextNumber // change than use(prefix)
+
+ console.log(b)
+
+let negNumber = 4
+
+let c = negNumber--
+
+console.log(c)
+
+let negNumber1 = 4
+
+let d = --negNumber1
+
+console.log(d)
