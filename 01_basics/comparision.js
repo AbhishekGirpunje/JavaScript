909f8ef2bc4d6a0d83aa7f2b13227489085cb1ca
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-13 01:23:46 +0000

    Comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,29 @@
+// console.log( 2 < 2);
+// console.log( 2 <= 2);
+// console.log( 2 > 2);
+// console.log( 2 >= 2);
+// console.log( 2 == 2);
+// console.log( 2 =! 2);
+
+
+
+// console.log("2" == 1);//o/p- false (comparision convert string into number)
+// console.log("1" == 1)///op-  true
+
+
+//console.log( null == 0);  //f
+//console.log( null > 0)    //f
+//console.log( null < 0)    //f
+//console.log( null >= 0)   //true
+//console.log( null <= 0)   // true
+
+
+console.log( undefined == 0);   //f
+console.log( undefined > 0) //f
+console.log( undefined < 0) //f
+console.log( undefined >= 0)    //f
+console.log( undefined <= 0)    //f
+
+//=== (strict check)
+
+console.log("1" === 1);//(It will check datatype)
