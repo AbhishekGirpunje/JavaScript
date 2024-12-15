b3f966cbced56d63b04a096090ac3f35476bef26
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-15 22:58:24 +0000

    Math Methods

---
+++ 01_basics/mathMethods.js
@@ -0,0 +1,44 @@
+//Math.abs()
+let num = -12;
+console.log(Math.abs(num));//12
+
+//Math.ceil()
+let num1 = 2.1;
+console.log(Math.ceil(num1));//3
+
+//Math.floor()
+let num2 = 2.9;
+console.log(Math.floor(num2));//2
+
+//Math.round()
+let num3 = 2.5;
+console.log(Math.round(num3));//3
+
+let num4 = 2.2;
+console.log(Math.round(num4));//2
+
+//Math.max()
+let num5 = Math.max(21,2,31,4,2,1,6);//31
+console.log(num5);
+
+//Math.min()
+let num6 = Math.min(31,2,3,7,4);//2
+console.log(num6);
+
+//Math.pow()
+let num7 = Math.pow(2, 3);//8
+console.log(num7);
+
+//Math.sqrt()
+let num8 = Math.sqrt(16);
+console.log(num8);
+
+//Math.random();
+let num9 = Math.ceil(Math.random()*10); //random number in between 1 and 10
+console.log(num9);
+
+let num10 = Math.random();// random number in between 0 and 1
+console.log(num10);
+
+
+
