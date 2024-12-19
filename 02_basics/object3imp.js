cb60a97801f99296794234bbaf31457c8b1eb637
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-19 01:49:35 +0000

    JSON and destructuring

---
+++ 02_basics/object3imp.js
@@ -0,0 +1,29 @@
+// destructuring
+
+const course  = {
+    course: "js by abhi",
+    price: 999,
+    courseInstructor: "Abhishek"
+}
+
+// console.log(course.courseInstructor)//Abhishek
+
+const {price: Rs} = course
+//console.log(courseInstructor);//Abhishek
+console.log(Rs)
+
+//APIs
+//JSON
+
+{
+    "name": "Abhishek",
+    "course": "js"
+    "price":"Free"
+}
+//OR
+[
+    {},
+    {},
+    {}
+]
+
