2610ed57d85e39e2f77f99fa0d53111a6eccb305
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-25 04:38:35 +0000

    if else condition

---
+++ controlFlow/one.js
@@ -0,0 +1,46 @@
+//if
+
+// const isLoggedIn = 2
+// const schoolAttendence = 72
+
+// if(schoolAttendence < 50){
+//     console.log("Less than 50")
+// }else{
+//     console.log("Greater than 50")
+// }
+
+
+
+// if(isLoggedIn === "2"){
+//     console.log("True");
+// }else{
+//     console.log("False")
+// }
+
+
+// const lvl = 200;
+
+// if(lvl > 100){
+//     const power = "fly"
+//     console.log(`your ability: ${power}`)
+// }
+
+//implicite scope
+
+const score = 100;
+
+//if(score < 200) console.log(`score is less than 200. current is ${score}`)
+
+// if(score < 2){
+//     console.log("less than 200")
+// }else if(score < 15){
+//     console.log("less than 150")
+// }else if(score < 50){
+//     console.log("less than 50")
+// }else if(score < 3){
+//     console.log("lesss than 300")
+// }else{
+//     console.log("So score is less than 100")
+// }
+
+
