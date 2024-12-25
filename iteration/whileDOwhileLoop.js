0a36657214fb107c92ac0faf3eb8059c2a12f7ea
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2024-12-25 08:15:20 +0000

    While do while loops

---
+++ iteration/whileDOwhileLoop.js
@@ -0,0 +1,22 @@
+let i = 0;
+
+while(i<=10){
+    console.log(`Value of i is ${i+1}`)
+    i = i+2;
+}
+
+let ArrNew = ["Abhi","sahil","rohit"]
+
+let arr = 0;
+
+while(arr < ArrNew.length){
+    console.log(`value is ${ArrNew[arr]}`)
+    arr = arr + 1;
+}
+
+let scoreVal = 1;
+
+do{
+    console.log(`score is ${scoreVal}`)
+    scoreVal++;
+}while(scoreVal <= 10)
